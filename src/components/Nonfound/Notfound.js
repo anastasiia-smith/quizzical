f55c008cbe247cd594a2bf0a11c5451168a6fc57
Anastasiia Smith <anastasiiasmithdev@gmail.com>
2023-08-21 17:20:01 +0300

    feat: add redirect from Notfound page.

--- src/components/Nonfound/Notfound.js
+++ src/components/Nonfound/Notfound.js
@@ -1,3 +1,9 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function Notfound() {
+  const navigate = useNavigate()
+  setTimeout(() => {
+    navigate('/quizzical')
+  }, 1000);
   return <h1 className='header'>Not found...</h1>;
 }
